a0d85528573dc7b9adbf45ecc76da91631e6c536
mato2512 <matoshri1420@gmail.com>
2025-10-28 20:03:30 +0530

    Hide Interface, Personalization, Audio, and Data Controls tabs from regular users

--- src/lib/components/chat/SettingsModal.svelte
+++ src/lib/components/chat/SettingsModal.svelte
@@ -637,29 +637,31 @@
 								<div class=" self-center">{$i18n.t('General')}</div>
 							</button>
 						{:else if tabId === 'interface'}
-							<button
-								role="tab"
-								aria-controls="tab-interface"
-								aria-selected={selectedTab === 'interface'}
-								class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
-								${
-									selectedTab === 'interface'
-										? ($settings?.highContrastMode ?? false)
-											? 'dark:bg-gray-800 bg-gray-200'
-											: ''
-										: ($settings?.highContrastMode ?? false)
-											? 'hover:bg-gray-200 dark:hover:bg-gray-800'
-											: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
-								}`}
-								on:click={() => {
-									selectedTab = 'interface';
-								}}
-							>
-								<div class=" self-center mr-2">
-									<AppNotification strokeWidth="2" />
-								</div>
-								<div class=" self-center">{$i18n.t('Interface')}</div>
-							</button>
+							{#if $user?.role === 'admin'}
+								<button
+									role="tab"
+									aria-controls="tab-interface"
+									aria-selected={selectedTab === 'interface'}
+									class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
+									${
+										selectedTab === 'interface'
+											? ($settings?.highContrastMode ?? false)
+												? 'dark:bg-gray-800 bg-gray-200'
+												: ''
+											: ($settings?.highContrastMode ?? false)
+												? 'hover:bg-gray-200 dark:hover:bg-gray-800'
+												: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
+									}`}
+									on:click={() => {
+										selectedTab = 'interface';
+									}}
+								>
+									<div class=" self-center mr-2">
+										<AppNotification strokeWidth="2" />
+									</div>
+									<div class=" self-center">{$i18n.t('Interface')}</div>
+								</button>
+							{/if}
 						{:else if tabId === 'connections'}
 							{#if $user?.role === 'admin' || ($user?.role === 'user' && $config?.features?.enable_direct_connections)}
 								<button
@@ -713,77 +715,83 @@
 								</button>
 							{/if}
 						{:else if tabId === 'personalization'}
-							<button
-								role="tab"
-								aria-controls="tab-personalization"
-								aria-selected={selectedTab === 'personalization'}
-								class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
-								${
-									selectedTab === 'personalization'
-										? ($settings?.highContrastMode ?? false)
-											? 'dark:bg-gray-800 bg-gray-200'
-											: ''
-										: ($settings?.highContrastMode ?? false)
-											? 'hover:bg-gray-200 dark:hover:bg-gray-800'
-											: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
-								}`}
-								on:click={() => {
-									selectedTab = 'personalization';
-								}}
-							>
-								<div class=" self-center mr-2">
-									<Face strokeWidth="2" />
-								</div>
-								<div class=" self-center">{$i18n.t('Personalization')}</div>
-							</button>
+							{#if $user?.role === 'admin'}
+								<button
+									role="tab"
+									aria-controls="tab-personalization"
+									aria-selected={selectedTab === 'personalization'}
+									class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
+									${
+										selectedTab === 'personalization'
+											? ($settings?.highContrastMode ?? false)
+												? 'dark:bg-gray-800 bg-gray-200'
+												: ''
+											: ($settings?.highContrastMode ?? false)
+												? 'hover:bg-gray-200 dark:hover:bg-gray-800'
+												: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
+									}`}
+									on:click={() => {
+										selectedTab = 'personalization';
+									}}
+								>
+									<div class=" self-center mr-2">
+										<Face strokeWidth="2" />
+									</div>
+									<div class=" self-center">{$i18n.t('Personalization')}</div>
+								</button>
+							{/if}
 						{:else if tabId === 'audio'}
-							<button
-								role="tab"
-								aria-controls="tab-audio"
-								aria-selected={selectedTab === 'audio'}
-								class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
-								${
-									selectedTab === 'audio'
-										? ($settings?.highContrastMode ?? false)
-											? 'dark:bg-gray-800 bg-gray-200'
-											: ''
-										: ($settings?.highContrastMode ?? false)
-											? 'hover:bg-gray-200 dark:hover:bg-gray-800'
-											: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
-								}`}
-								on:click={() => {
-									selectedTab = 'audio';
-								}}
-							>
-								<div class=" self-center mr-2">
-									<SoundHigh strokeWidth="2" />
-								</div>
-								<div class=" self-center">{$i18n.t('Audio')}</div>
-							</button>
-						{:else if tabId === 'data_controls'}
-							<button
-								role="tab"
-								aria-controls="tab-data-controls"
-								aria-selected={selectedTab === 'data_controls'}
-								class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
-								${
-									selectedTab === 'data_controls'
-										? ($settings?.highContrastMode ?? false)
-											? 'dark:bg-gray-800 bg-gray-200'
-											: ''
-										: ($settings?.highContrastMode ?? false)
-											? 'hover:bg-gray-200 dark:hover:bg-gray-800'
-											: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
-								}`}
-								on:click={() => {
-									selectedTab = 'data_controls';
-								}}
-							>
-								<div class=" self-center mr-2">
-									<DatabaseSettings strokeWidth="2" />
-								</div>
-								<div class=" self-center">{$i18n.t('Data Controls')}</div>
-							</button>
+							{#if $user?.role === 'admin'}
+								<button
+									role="tab"
+									aria-controls="tab-audio"
+									aria-selected={selectedTab === 'audio'}
+									class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
+									${
+										selectedTab === 'audio'
+											? ($settings?.highContrastMode ?? false)
+												? 'dark:bg-gray-800 bg-gray-200'
+												: ''
+											: ($settings?.highContrastMode ?? false)
+												? 'hover:bg-gray-200 dark:hover:bg-gray-800'
+												: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
+									}`}
+									on:click={() => {
+										selectedTab = 'audio';
+									}}
+								>
+									<div class=" self-center mr-2">
+										<SoundHigh strokeWidth="2" />
+									</div>
+									<div class=" self-center">{$i18n.t('Audio')}</div>
+								</button>
+							{/if}
+						{:else if tabId === 'data_controls'}}
+							{#if $user?.role === 'admin'}
+								<button
+									role="tab"
+									aria-controls="tab-data-controls"
+									aria-selected={selectedTab === 'data_controls'}
+									class={`px-0.5 md:px-2.5 py-1 min-w-fit rounded-xl flex-1 md:flex-none flex text-left transition
+									${
+										selectedTab === 'data_controls'
+											? ($settings?.highContrastMode ?? false)
+												? 'dark:bg-gray-800 bg-gray-200'
+												: ''
+											: ($settings?.highContrastMode ?? false)
+												? 'hover:bg-gray-200 dark:hover:bg-gray-800'
+												: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'
+									}`}
+									on:click={() => {
+										selectedTab = 'data_controls';
+									}}
+								>
+									<div class=" self-center mr-2">
+										<DatabaseSettings strokeWidth="2" />
+									</div>
+									<div class=" self-center">{$i18n.t('Data Controls')}</div>
+								</button>
+							{/if}
 						{:else if tabId === 'account'}
 							<button
 								role="tab"
