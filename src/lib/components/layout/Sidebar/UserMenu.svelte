9418f526c55909645d7089b596b1f144f86df259
mato2512 <mato2512@github.com>
2025-11-02 00:23:00 +0530

    Update profile icon to cleaner design in user menu

--- src/lib/components/layout/Sidebar/UserMenu.svelte
+++ src/lib/components/layout/Sidebar/UserMenu.svelte
@@ -73,8 +73,8 @@
 			<!-- User Email Display -->
 			<div class="px-3 py-2 border-b border-gray-100 dark:border-gray-800">
 				<div class="flex items-center gap-2 text-xs text-gray-500 dark:text-gray-400">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-4">
-						<path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+						<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
 					</svg>
 					<span class="truncate">{$user?.email}</span>
 				</div>
