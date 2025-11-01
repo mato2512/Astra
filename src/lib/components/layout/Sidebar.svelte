a43017f7bb19f6056704d09f27d8e948ac16c6a9
mato2512 <matoshri1420@gmail.com>
2025-11-02 00:02:12 +0530

    Move user email to dropdown menu in sidebar

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -1206,17 +1206,6 @@
 				></div>
 				<div class="flex flex-col font-primary">
 					{#if $user !== undefined && $user !== null}
-						<!-- User Email Display -->
-						<div class="px-3 py-2 mb-1">
-							<div class="flex items-center gap-2 text-xs text-gray-500 dark:text-gray-400">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-4">
-									<path d="M3 4a2 2 0 0 0-2 2v1.161l8.441 4.221a1.25 1.25 0 0 0 1.118 0L19 7.162V6a2 2 0 0 0-2-2H3Z" />
-									<path d="m19 8.839-7.77 3.885a2.75 2.75 0 0 1-2.46 0L1 8.839V14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V8.839Z" />
-								</svg>
-								<span class="truncate">{$user?.email}</span>
-							</div>
-						</div>
-
 						<UserMenu
 							role={$user?.role}
 							on:show={(e) => {
