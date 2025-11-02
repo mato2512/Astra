7f9cc4cc4a8a5d4baa415746d78f7d3642bf73ad
mato2512 <mato2512@github.com>
2025-11-02 13:22:42 +0530

    Update Placeholder to bottom layout on mobile

--- src/lib/components/chat/Placeholder.svelte
+++ src/lib/components/chat/Placeholder.svelte
@@ -17,7 +17,8 @@
 		temporaryChatEnabled,
 		selectedFolder,
 		chats,
-		currentChatPage
+		currentChatPage,
+		mobile
 	} from '$lib/stores';
 	import { sanitizeResponseContent, extractCurlyBraceWords } from '$lib/utils';
 	import { WEBUI_BASE_URL } from '$lib/constants';
@@ -109,7 +110,7 @@
 	<div
 		class="w-full text-3xl text-gray-800 dark:text-gray-100 text-center flex items-center gap-4 font-primary"
 	>
-		<div class="w-full flex flex-col justify-center items-center">
+		<div class="w-full flex flex-col {$mobile ? 'justify-end' : 'justify-center'} items-center">
 			{#if $selectedFolder}
 				<FolderTitle
 					folder={$selectedFolder}
